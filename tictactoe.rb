f90b522a1a41bea7c9a07fcb825a93f58427d3a8
Doug Heatter <drheatter@gmail.com>
2019-05-12 13:08:41 -0400

    Added Player class.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,23 @@
 class Cell
-	attr_reader :symbol, :played
+  attr_reader :symbol, :played
 
-	def initialize
-		@symbol = " "
-		@used = false
-	end
+  def initialize
+    @symbol = " "
+    @used = false
+  end
 
-	def fill_cell(symbol_input)
-		return if used
-		@symbol = symbol_input
-		@used = true
-	end
+  def fill_cell(symbol_input)
+  return if used
+  @symbol = symbol_input
+  @used = true
+  end
+end
+
+class Player
+	attr_reader :player, :symbol
+
+  def initialize(name, symbol)
+  	@name = name
+  	@symbol = symbol
+  end
 end
